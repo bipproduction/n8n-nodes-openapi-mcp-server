8f1be581c6d4486a072ebc10fd00bd3a79ed04e8
bipproduction <bip.production.js@gmail.com>
2025-11-17 14:11:01 +0800

    build

--- bin/publish.ts
+++ bin/publish.ts
@@ -1,6 +1,3 @@
 import { execSync } from "child_process";
 
-execSync("git add -A", { stdio: 'inherit' })
-execSync("git commit -m 'publish'", { stdio: 'inherit' })
-execSync("git push", { stdio: 'inherit' })
 execSync("cd dist && npm publish", { stdio: 'inherit' })
