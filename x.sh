b2bb780a5a895fe50a0c415aa524c5c777ba0637
bipproduction <bip.production.js@gmail.com>
2025-11-19 11:15:11 +0800

    build

---
+++ x.sh
@@ -0,0 +1,6 @@
+FAHMI=628123833845
+JUNAIDIL=62811380873
+curl --get \
+  --data-urlencode "nom=$JUNAIDIL" \
+  --data-urlencode "text=ngetes doang gak usah dibales" \
+  https://wa.wibudev.com/code
